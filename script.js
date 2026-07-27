a31a84d14ff64ed6f46db4e21b124602e7ada5e7
FUSION ENTERTAINMENT <santinosimon@oianguren.eu>
2026-07-27 02:50:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,101 @@ alert(
 );
 
 }
+
+
+function start3DViewer(){
+
+const container = document.getElementById("viewer3d");
+
+if(!container) return;
+
+
+// Escena
+const scene = new THREE.Scene();
+
+
+// Cámara
+const camera = new THREE.PerspectiveCamera(
+45,
+container.clientWidth / container.clientHeight,
+0.1,
+1000
+);
+
+
+// Render
+const renderer = new THREE.WebGLRenderer({
+antialias:true
+});
+
+
+renderer.setSize(
+container.clientWidth,
+container.clientHeight
+);
+
+
+container.innerHTML="";
+
+container.appendChild(renderer.domElement);
+
+
+// Luz
+
+const light = new THREE.DirectionalLight(
+0xffffff,
+2
+);
+
+light.position.set(5,5,5);
+
+scene.add(light);
+
+
+// Modelo temporal cubo Minecraft
+
+const geometry =
+new THREE.BoxGeometry(2,2,2);
+
+
+const material =
+new THREE.MeshStandardMaterial({
+color:0x00ff88
+});
+
+
+const cube =
+new THREE.Mesh(
+geometry,
+material
+);
+
+
+scene.add(cube);
+
+
+
+camera.position.z=5;
+
+
+
+function animate(){
+
+requestAnimationFrame(animate);
+
+cube.rotation.y+=0.01;
+
+renderer.render(
+scene,
+camera
+);
+
+}
+
+
+animate();
+
+}
+
+
+window.onload=start3DViewer;
